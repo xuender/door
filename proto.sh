641aeec724bc364f9386296282972d8ae81b821f
徐晨阳 <xuender@gmail.com>
2017-06-22 16:15:00 +0800

    proto

---
+++ proto.sh
@@ -0,0 +1,8 @@
+# protoc \
+# --plugin=protoc-gen-ts=/home/ender/.nvm/versions/node/v6.10.2/bin/protoc-gen-ts \
+# --js_out=import_style=commonjs,binary:generated \
+# --ts_out=service=true:generated \
+# -I ./proto \
+# proto/*.proto
+
+protoc --go_out=./example/chat -I ./example/chat ./example/chat/*.proto
