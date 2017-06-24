517d6f1f61e6460351435ff89151e74d842638ea
徐晨阳 <xuender@gmail.com>
2017-06-24 09:58:23 +0800

    example

--- example/main.go
+++ example/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"time"
 
 	"./chat"
@@ -20,11 +19,10 @@ func main() {
 	d.PUT("nick", nick)
 
 	e := echo.New()
-	e.GET("/", html)
 	e.GET("/ws", func(c echo.Context) error {
 		return d.WebsocketHandler(c.Response().Writer, c.Request())
 	})
-	e.Static("/static", "static")
+	e.Static("/", "dist")
 	e.Logger.Fatal(e.Start(":8888"))
 }
 
@@ -69,19 +67,4 @@ func open(c door.Context) error {
 	}
 	c.Revert(ca, door.MethodEnum_POST, "send")
 	return nil
-}
-
-func html(c echo.Context) error {
-	return c.HTML(http.StatusOK, `<!doctype html>
-<html lang="en">
-<head>
-  <meta charset="utf-8">
-  <title>聊天室</title>
-  <base href="/">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="static/styles.d41d8cd98f00b204e980.bundle.css" rel="stylesheet"/></head>
-<body>
-  <app-root></app-root>
-<script type="text/javascript" src="static/inline.dc6cb9009bfb41fbc468.bundle.js"></script><script type="text/javascript" src="static/polyfills.5ca19fb28cd74641de8d.bundle.js"></script><script type="text/javascript" src="static/vendor.75eee41300e46be93317.bundle.js"></script><script type="text/javascript" src="static/main.12822489372e81981fb9.bundle.js"></script></body>
-</html>`)
 }
